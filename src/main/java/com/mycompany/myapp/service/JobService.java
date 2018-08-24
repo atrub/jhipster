9996d6bea0999680ff3f41244facb24b8a2f6375
admin <a_trub@mail.ru>
2018-08-24 10:40:06 +0500

    commit

---
+++ src/main/java/com/mycompany/myapp/service/JobService.java
@@ -0,0 +1,52 @@
+package com.mycompany.myapp.service;
+
+import com.mycompany.myapp.domain.Job;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+/**
+ * Service Interface for managing Job.
+ */
+public interface JobService {
+
+    /**
+     * Save a job.
+     *
+     * @param job the entity to save
+     * @return the persisted entity
+     */
+    Job save(Job job);
+
+    /**
+     * Get all the jobs.
+     *
+     * @param pageable the pagination information
+     * @return the list of entities
+     */
+    Page<Job> findAll(Pageable pageable);
+
+    /**
+     * Get all the Job with eager load of many-to-many relationships.
+     *
+     * @return the list of entities
+     */
+    Page<Job> findAllWithEagerRelationships(Pageable pageable);
+    
+    /**
+     * Get the "id" job.
+     *
+     * @param id the id of the entity
+     * @return the entity
+     */
+    Optional<Job> findOne(Long id);
+
+    /**
+     * Delete the "id" job.
+     *
+     * @param id the id of the entity
+     */
+    void delete(Long id);
+}
